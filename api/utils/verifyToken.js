46f3dde6649de63856e13af17837a17990a23489
Daisy Le <daisy.le0203@gmail.com>
2023-01-08 23:41:40 -0600

    create error and verify token middleware

---
+++ api/utils/verifyToken.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken"
+import { createError } from "../utils/error.js"
+
+// verifyToken middleware
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token
+  // if there is no token stored in cookies, the request is unauthorized
+  if (!token) {
+    return next(createError(401, "You are not authenticated!"))
+  }
+
+  // if there is a token, we will verify it and get the user information
+  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+    if (err) return next(createError(403, "Token is not valid!"))
+    // if everything is okay, set new request property which is user new information and go to next operation
+    req.user = user
+    next()
+  })
+}
+
