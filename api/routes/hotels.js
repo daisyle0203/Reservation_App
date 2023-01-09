526d17fd053395f987c9202d5b84d009b4a2c947
Daisy Le <daisy.le0203@gmail.com>
2023-01-08 20:53:44 -0600

    add put route for hotel

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -5,18 +5,29 @@ const router = express.Router()
 
 //CREATE
 router.post("/", async (req, res) => {
-    const newHotel = new Hotel(req.body)
+  const newHotel = new Hotel(req.body)
 
-    try {
-        const savedHotel = await newHotel.save()
-        res.status(200).json(savedHotel)
-    } catch (err) {
-        res.status(500).json(err)
-    }
-} )
+  try {
+    const savedHotel = await newHotel.save()
+    res.status(200).json(savedHotel)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
 
 //UPDATE
-// router.put("/:id", )
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    )
+    res.status(200).json(updatedHotel)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
 //DELETE
 // router.delete("/:id", )
 //GET
