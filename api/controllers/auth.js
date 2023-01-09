18c0b2f663e378b08a3bb6cc410ff71219312375
Daisy Le <daisy.le0203@gmail.com>
2023-01-08 21:57:21 -0600

    add bcrypt to hash password

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,11 +1,15 @@
 import User from "../models/User.js"
+import bcrypt from "bcryptjs"
 
 export const signup = async (req, res) => {
   try {
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hashedPassword,
     })
 
     await newUser.save()
