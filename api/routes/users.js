8e8148a53de0a4ad0c3c2db776c247eb819072c5
Daisy Le <daisy.le0203@gmail.com>
2023-01-08 23:42:14 -0600

    add check user route with verify token middleware

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,8 +5,13 @@ import {
   getUser,
   getUsers,
 } from "../controllers/user.js"
+import { verifyToken } from "../utils/verifyToken.js"
 
 const router = express.Router()
+
+router.get("/checkuser", verifyToken, (req,res,next)=>{
+  res.send("hello user, you are logged in")
+})
 
 //UPDATE ONE USER
 router.put("/:id", updateUser)
