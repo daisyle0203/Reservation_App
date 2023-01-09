4d91de5dab5e074319c5ce955aec716bfe0c102f
Daisy Le <daisy.le0203@gmail.com>
2023-01-08 22:10:38 -0600

    add login route

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -3,8 +3,8 @@ import bcrypt from "bcryptjs"
 
 export const signup = async (req, res) => {
   try {
-    const salt = bcrypt.genSaltSync(10);
-    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+    const salt = bcrypt.genSaltSync(10)
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt)
 
     const newUser = new User({
       username: req.body.username,
@@ -18,3 +18,20 @@ export const signup = async (req, res) => {
     res.status(500).json(error)
   }
 }
+
+export const login = async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username })
+    if (!user) return res.status(404).send("User not found")
+
+    const isPasswordValid = await bcrypt.compare(req.body.password, user.password )
+    if (!isPasswordValid) return res.status(400).json("Wrong credentials")
+
+    const { password, isAdmin, ...others } = user._doc
+
+    res.status(200).json({...others})
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+ 
