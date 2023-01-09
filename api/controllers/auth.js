80af41d310599ddb55101a24af41d7fbd3055d47
Daisy Le <daisy.le0203@gmail.com>
2023-01-08 22:20:45 -0600

    add json web token and cookie when user is logged in

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js"
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 export const signup = async (req, res) => {
   try {
@@ -24,14 +25,27 @@ export const login = async (req, res) => {
     const user = await User.findOne({ username: req.body.username })
     if (!user) return res.status(404).send("User not found")
 
-    const isPasswordValid = await bcrypt.compare(req.body.password, user.password )
+    const isPasswordValid = await bcrypt.compare(
+      req.body.password,
+      user.password
+    )
     if (!isPasswordValid) return res.status(400).json("Wrong credentials")
+
+    const token = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.SECRET_KEY,
+      { expiresIn: "5d" }
+    )
 
     const { password, isAdmin, ...others } = user._doc
 
-    res.status(200).json({...others})
+    res
+      .cookie("access_token", token, {
+        httpOnly: true, // it doesn't allow any client secret to access the cookie
+      })
+      .status(200)
+      .json({ ...others })
   } catch (error) {
     res.status(500).json(error)
   }
 }
- 
