82cb245c7873c1c68f24170613a895d11435658b
Daisy Le <daisy.le0203@gmail.com>
2023-01-08 23:59:00 -0600

    add verify token for admin and add comments

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,18 +5,24 @@ import {
   getUser,
   getUsers,
 } from "../controllers/user.js"
-import { verifyToken, verifyUser } from "../utils/verifyToken.js"
+import { verifyToken, verifyUser, verifyAdmin } from "../utils/verifyToken.js"
 
 const router = express.Router()
 
-router.get("/checkuser", verifyToken, (req,res,next)=>{
+// CHECK IF USER IS LOGGED IN WITH VERIFY TOKEN
+router.get("/checkuser", verifyToken, (req, res, next) => {
   res.send("hello user, you are logged in")
 })
 
-router.get("/checkuser/:id", verifyUser, (req,res,next)=>{
+// CHECK IF USER IS LOGGED IN WITH VERIFY USER TOKEN
+router.get("/checkuser/:id", verifyUser, (req, res, next) => {
   res.send("hello user, you are logged in and you can delete your account")
 })
 
+// CHECK IF USER IS LOGGED IN WITH VERIFY ADMIN TOKEN
+router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+  res.send("hello admin, you are logged in and you can delete all accounts")
+})
 
 //UPDATE ONE USER
 router.put("/:id", updateUser)
