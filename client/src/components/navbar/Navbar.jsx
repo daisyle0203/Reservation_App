e8476be1aa9898226f9b286647b16df8865fe7d8
Daisy Le <daisy.le0203@gmail.com>
2023-02-09 22:38:37 -0600

    turn the logo to clickable logo

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom"
 import "./navbar.css"
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
+        <Link to="/" className="logo" style={{color: "inherit", textDecoration: "none"}}
+        >
         <span className="logo">DaisyBooking</span>
+        </Link>
         <div className="navItems">
           <button className="navButton">Sign up</button>
           <button className="navButton">Login</button>
