8e5bd5dcb1233e573c0c7f9e1cb35ec8fd5a2ce7
Daisy Le <daisy.le0203@gmail.com>
2023-02-09 22:35:09 -0600

    add queries to getHotels route

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -42,8 +42,13 @@ export const getHotel = async (req, res, next) => {
 }
 
 export const getHotels = async (req, res, next) => {
+  // destructure the query object to get the min and max price values and the rest of the query object
+  const { min, max, ...others } = req.query
   try {
-    const hotels = await Hotel.find()
+    const hotels = await Hotel.find({
+      ...others,
+      cheapestPrice: { $gte: min, $lte: max || 999 },
+    }).limit(req.query.limit)
     res.status(200).json(hotels)
   } catch (error) {
     next(error)
