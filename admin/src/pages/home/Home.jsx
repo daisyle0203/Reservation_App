b2e4928bb1e686b19be494064c5ba0fc9ed39e54
Daisy Le <daisy.le0203@gmail.com>
2023-02-12 15:53:19 -0600

    add navbar to the home page

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,8 +1,11 @@
+import Navbar from "../../components/navbar/Navbar"
 import "./home.scss"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Navbar />
+    </div>
   )
 }
 
