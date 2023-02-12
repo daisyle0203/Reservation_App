7739437d90274a132ef12e7ae0403bd892a702a8
Daisy Le <daisy.le0203@gmail.com>
2023-02-12 17:11:01 -0600

    add font

--- admin/src/components/sidebar/Sidebar.jsx
+++ admin/src/components/sidebar/Sidebar.jsx
@@ -18,7 +18,7 @@ const Sidebar = () => {
     <div className="sidebar">
       <div className="top">
         <Link to="/" style={{ textDecoration: "none" }}>
-          <span className="logo">Daisy Admin</span>
+          <span className="logo">StayZen</span>
         </Link>
       </div>
       <hr />
